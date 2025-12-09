a69b048e0ff12e3ea4a2ac2e28f1ebefffd8a496
Tirthankar Pal <141252995+Tirthankarpal@users.noreply.github.com>
2025-12-09 17:51:29 +0530

    Time: 1505 ms (10.21%) | Memory: 406 MB (54.74%) - LeetSync

---
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    typedef long long ll;
+    ll mod=1000000007;
+    int specialTriplets(vector<int>& a) {
+        map<ll,ll> mpk;
+        map<ll,ll> mpj;
+        ll ans=0;
+        for(int i=a.size()-1;i>=0;i--){
+            if(a[i]%2==0){
+                ans+=(mpj[a[i]/2])%mod;
+                ans%=mod;
+            }
+            mpj[a[i]]+=mpk[2*a[i]];
+            mpk[a[i]]++;
+        }
+        return ans;
+    }
+};
