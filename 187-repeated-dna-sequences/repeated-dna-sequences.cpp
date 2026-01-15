56d8bb42f57ae63a3f5b073e22d79243495440db
Tirthankar Pal <141252995+Tirthankarpal@users.noreply.github.com>
2026-01-15 21:34:15 +0530

    Time: 39 ms (63.82%) | Memory: 26.4 MB (7.46%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/repeated-dna-sequences.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        map<string,int> mp;
+        unordered_map<string,int> mp;
         for(int i=0;i<max((int)s.size()-9,0);i++){
             mp[s.substr(i,10)]++;
         }
